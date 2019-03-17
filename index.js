707d74e7bb235298582f10a18f1aa36e0ae0bfaa
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-17 20:58:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(driver => driver.revenue > revenue);
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers.filter(driver => driver.revenue > revenue).map(driver => `${driver.name}`);
+}
